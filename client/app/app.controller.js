f3bae1c6fb521b2add8d760cf9cbc804181636ab
Victor York <victor.a.york@gmail.com>
2016-01-09 03:52:31 -0600

    Add more filler content on the home page.

---
+++ client/app/app.controller.js
@@ -0,0 +1,9 @@
+"use strict";
+
+angular.module('vicyork')
+  .controller('Checker', ['$location', '$scope', function($location, $scope){
+    //$scope.check = $location.path() === '/' ? true : false;
+
+    //$scope.currentPath = $location.path();
+
+  }]);
--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,4 +1,14 @@
- angular.module('vicyork')
-    .controller('HomeController', ['$scope', function($scope) {
-      $scope.yo = 'home is here on scope';
-    }]);
+angular.module('vicyork')
+  .controller('HomeController', ['$scope', '$rootScope', '$location', '$log', function($scope, $rootScope, $location, $log) {
+    $scope.yo = 'home is here on scope';
+
+    $log.info($location);
+
+    $log.log($location.path());
+
+    if ($location.path() !== '/') {
+      $scope.check = false;
+    }
+
+
+  }]);
